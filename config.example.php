02bab5e2a26d78d4811029673e5fad7233591bbb
Thomasmuur <thomasfrij@gmail.com>
2021-01-15 14:00:45 +0100

    initial commit

---
+++ config.example.php
@@ -0,0 +1,3 @@
+<?php
+
+$_ENV['COMPANY_NAME'] = 'Your company';
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require('config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?= $_ENV['COMPANY_NAME']; ?></title>
+  </head>
+  <body>
+
+  </body>
+</html>
